c9b4382bbb7041c319017d7ac314ef5e998a930a
Kamil Kacperczyk <Kacperczyk95@gmail.com>
2023-07-31 23:00:15 +0200

    Project initial state

---
+++ File-reader/README.md
@@ -0,0 +1,9 @@
+1) Upon launching it shows list of columns of selected file.
+2) The user is able to choose the columns of interest.
+3) After selecting one column, they must decide whether to choose another.
+3) a) If yes, then the chosen column is removed from the pool of available columns.
+   The selected column is added to an unseen list, which stores the user's choices.
+   This will enable later loading of the file with only the chosen columns.
+3) b) If the user enters "STOP," the loop is interrupted, and we proceed to step 4).
+4) We choose what to do next.
+4) a) Loading a file with the selected columns.
---
+++ File-reader/main.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('Persons_creator.txt', 'r') as file_r:
+    csv_reader = csv.DictReader(file_r)
+
+    available_columns = csv_reader.fieldnames
+    all_chosen_columns = []
+
+    while True:
+        chosen_column = input(f"Proszę wybrać kolumny lub wpisać STOP:\n{available_columns}: ")
+        if chosen_column in available_columns:
+            available_columns.remove(chosen_column)
+            all_chosen_columns.append(chosen_column)
+        elif chosen_column.lower() == 'stop':
+            break
+        else:
+            print("Nie ma takiej kolumny!")
+        if not available_columns:
+            print("Wyczerpałeś dostępne kolumny.")
+            break
+
+    print(f"\nWybrane kolumny to: {all_chosen_columns}")
+
+    read_or_what = input("\nCo chcesz teraz zrobić?: \n1) Wczytaj wybrane kolumny z pliku\nOpcja nr: ")
+    if read_or_what.lower() == '1':
+
+        with open('Persons_creator.txt', 'r') as file_r2:
+            csv_reader = csv.DictReader(file_r2)
+            for row in csv_reader:
+                selected_data = [row[column] for column in all_chosen_columns]
+                print(', '.join(selected_data))
